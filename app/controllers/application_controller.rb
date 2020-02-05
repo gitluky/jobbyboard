ee326588f1ee53f2c5facc9c13bc6d9028ac5905
gitluky <jluk87@gmail.com>
2020-02-04 19:47:50 -0500

    Ran 'rails generate devise:install', added 'config.action_mailer.default_url_options = { host: 'localhost', port: 3001 }' to config/environments/development.rb file. Added 'application#index' as the root path. Updated config/puma.rb to fetch from port 3001. Updated application.rb with configurations for rack-cors to allow from 'http://localhost:3001' origin.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::API
+
+  def index
+
+  end
+  
 end
--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,17 @@ module Jobbyboard
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    Rails.application.config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'http://localhost:3000'
+
+        resource '*',
+          headers: :any,
+          methods: [:get, :post, :put, :patch, :delete, :options, :head],
+          credentials: true
+      end
+    end
+    
   end
 end
